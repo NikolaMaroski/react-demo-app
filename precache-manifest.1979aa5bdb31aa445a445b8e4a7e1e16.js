3ef4d6dca994af8eb6dec1654185e726ccd7feb2
Nikola Maroski <nikolamaroski@hotmail.com>
2019-12-01 17:49:35 +0100

    Updates

---
+++ precache-manifest.1979aa5bdb31aa445a445b8e4a7e1e16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab581bd73da926b45392463395793124",
+    "url": "/react-demo-app/index.html"
+  },
+  {
+    "revision": "bd6c403efcdf73ffac9f",
+    "url": "/react-demo-app/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "b0eb0bdb47f2891cc627",
+    "url": "/react-demo-app/static/css/main.e96e9a55.chunk.css"
+  },
+  {
+    "revision": "bd6c403efcdf73ffac9f",
+    "url": "/react-demo-app/static/js/2.0e9148a5.chunk.js"
+  },
+  {
+    "revision": "b0eb0bdb47f2891cc627",
+    "url": "/react-demo-app/static/js/main.a819dfee.chunk.js"
+  },
+  {
+    "revision": "f35b956b5dc165dc5888",
+    "url": "/react-demo-app/static/js/runtime-main.24b8617c.js"
+  }
+]);
